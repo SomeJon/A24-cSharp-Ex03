d93f370fd81da07b42575c49483c3f027fa37b1e
Jonathan Shaya <jonathansh2@mta.ac.il>
2024-02-28 12:04:56 +0200

    Close to finished

--- Ex03.GarageLogic/FrequantActions.cs
+++ Ex03.GarageLogic/FrequantActions.cs
@@ -22,5 +22,19 @@ namespace Ex03.GarageLogic
                 throw new ValueOutOfRangeException(errorMessage, i_MaxValue, i_MinValue);
             }
         }
+
+        public static void EnterintValueInRange(ref int o_Value, int i_Value, string i_ValueName, int i_MaxValue, int i_MinValue = 0)
+        {
+            if (i_Value <= i_MaxValue && i_Value >= i_MinValue)
+            {
+                o_Value = i_Value;
+            }
+            else
+            {
+                string errorMessage =
+                    string.Format("Out of possible capacity for {0}! Capacity of {0} is: {1} - {2}", i_ValueName, i_MinValue, i_MaxValue);
+                throw new ValueOutOfRangeException(errorMessage, i_MaxValue, i_MinValue);
+            }
+        }
     }
 }
