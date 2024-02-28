3e1c702ca629021b49e877a7643a796ebc26d276
Jonathan Shaya <jonathansh2@mta.ac.il>
2024-02-28 10:43:08 +0200

    Seventh batch' i hate writing ui

--- Ex03.GarageLogic/GarageData.cs
+++ Ex03.GarageLogic/GarageData.cs
@@ -11,14 +11,37 @@ namespace Ex03.GarageLogic
         private const int k_VehicleNotInGarage = -1; 
         private  List<GarageVehicleCard> m_ListOfVehicles = new List<GarageVehicleCard>();
 
+        public List<GarageVehicleCard> ListOfVehicles
+        {
+            get { return m_ListOfVehicles; }
+        }
+
         public void AddCardToList(GarageVehicleCard i_CardToAdd)
         {
             m_ListOfVehicles.Add(i_CardToAdd);
         }
         
-        private GarageVehicleCard FindVehicle(string i_LicensePlate)
+        public GarageVehicleCard FindVehicle(string i_LicensePlate)
         {
             return m_ListOfVehicles.Find(card => (card.CardVehicle == i_LicensePlate)); ;
+        }
+
+        public List<string> FindAllMatchLicences(GarageVehicleCard.eVehicleStatus i_WantedStatus)
+        {
+            List<GarageVehicleCard> Matches = m_ListOfVehicles.FindAll(card => (card.VehicleStatus == i_WantedStatus));
+            return GetLicences(Matches);
+        }
+
+        public static List<string> GetLicences(List<GarageVehicleCard> i_WantedList)
+        {
+            List<string> licences = new List<string>();
+
+            foreach (GarageVehicleCard Card in i_WantedList)
+            {
+                licences.Add(Card.CardVehicle.LicensePlate);
+            }
+
+            return licences;
         }
 
         public bool isVehicleInGarage(string i_LicensePlate)
